1e510d210e11e281f69a887926e384731348928e
Rishikesh Vishwakarma <rishikeshv9693@gmail.com>
2023-05-31 21:47:25 +0530

    add palindrome partitioning

---
+++ leetcode/leetcode/131_palidrome_partitioning.go
@@ -0,0 +1,41 @@
+package leetcode
+
+import (
+	"fmt"
+	"queue/queue"
+)
+
+func PalindromePartitioning(s string) {
+	var ans [][]string
+	var q queue.Queue[string]
+	var f func(int)
+
+	f = func(index int) {
+		if index == len(s) {
+			ans = append(ans, q)
+			return
+		}
+
+		for i := index; i < len(s); i++ {
+			if isValidPalindrome(s[index : i+1]) {
+				q.Push(s[index : i+1])
+				f(i + 1)
+				q.Pop()
+			}
+		}
+	}
+
+	f(0)
+
+	fmt.Println(ans)
+}
+
+func isValidPalindrome(s string) bool {
+	N := len(s)
+	for i := 0; i < N/2; i++ {
+		if s[i] != s[N-i-1] {
+			return false
+		}
+	}
+	return true
+}
